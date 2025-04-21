464bfb28e7a1787ebf14fc7084b30f6a3a759f31
N0vl1e <an5226466@gmail.com>
2025-04-21 09:28:35 +0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,5 @@
 function toggleDropdown(event, id) {
   event.preventDefault();
-  // Закрыть все открытые дропдауны
   document.querySelectorAll(".dropdown-content").forEach((drop) => {
     if (drop.id !== id) {
       drop.classList.remove("show");
@@ -44,3 +43,19 @@ function showNextSlide() {
 }
 
 setInterval(showNextSlide, 3000);
+
+
+let btns = document.querySelectorAll('.btn-filter')
+let images = document.querySelectorAll('.about-img')
+let texts = document.querySelectorAll('.about-other-block')
+
+btns.forEach((btn, index)=>{
+  btn.onclick=()=>{
+    document.querySelector('.btn-filter.active').classList.remove('active')
+    btn.classList.add('active')
+    document.querySelector('.about-img.active').classList.remove('active')
+    images[index].classList.add('active')
+    document.querySelector('.about-other-block.active').classList.remove('active')
+    texts[index].classList.add('active')
+  }
+})
